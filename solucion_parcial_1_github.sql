a931d6e1d422d56fbb6ececc422d92750ebf056e
leantroca <67613109+leantroca@users.noreply.github.com>
2021-05-25 17:56:02 -0300

    Update solucion_parcial_1_github.sql

--- solucion_parcial_1_github.sql
+++ solucion_parcial_1_github.sql
@@ -1,33 +1,12 @@
-delete from alquila where id_oficina > 0;
-delete from oficina where id_oficina > 0;
-delete from cliente where nro_doc > 0;
-delete from sala_convencion where id_oficina > 0;
-
-copy oficina from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/oficina.csv' with (format csv);
-copy cliente from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/cliente.csv' with (format csv);
-copy alquila from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/alquila.csv' with (format csv);
-copy sala_convencion from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/sala_convencion.csv' with (format csv);
-
-
-select o.id_oficina, o.cant_max_personas, o.monto_alquiler
-from oficina o 
-where o.id_oficina < 5
-	and o.id_oficina = any(
-		select a.id_oficina 
-		from alquila a
-		group by a.id_oficina
-		having count(*) >= 30
-	);
-
-------------------------------------------------------------------------------------
+-- Ej 1-5
 
 delete from participa where id_evento > 0;
 delete from evento where id_evento > 0;
 delete from modelo where nro_doc > 0;
 
-copy evento from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/evento.csv' with (format csv);
-copy modelo from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/modelo.csv' with (format csv);
-copy participa from 'https://raw.githubusercontent.com/leantroca/basesdedatos.tudai2021/main/participa.csv' with (format csv);
+copy evento from 'evento.csv' with (format csv);
+copy modelo from 'modelo.csv' with (format csv);
+copy participa from 'participa.csv' with (format csv);
 
 select e.id_evento, e.fecha_evento , e.direccion , e.ciudad 
 from evento e 
@@ -39,3 +18,26 @@ where e.id_evento = any(
 	group by p.id_evento 
 	having count(p.nro_doc) > 5
 );
+
+-- Ej 1-6
+
+delete from alquila where id_oficina > 0;
+delete from oficina where id_oficina > 0;
+delete from cliente where nro_doc > 0;
+delete from sala_convencion where id_oficina > 0;
+
+copy oficina from 'oficina.csv' with (format csv);
+copy cliente from 'cliente.csv' with (format csv);
+copy alquila from 'alquila.csv' with (format csv);
+copy sala_convencion from 'sala_convencion.csv' with (format csv);
+
+
+select o.id_oficina, o.cant_max_personas, o.monto_alquiler
+from oficina o 
+where o.id_oficina < 5
+	and o.id_oficina = any(
+		select a.id_oficina 
+		from alquila a
+		group by a.id_oficina
+		having count(*) >= 30
+	);
